087f477adc05418b3977287f558b9e2fca5eb665
Maycon Alexsander da Silva <mayconalexm@gmail.com>
2026-07-13 22:31:16 -0300

    :recycle: refactor: refatoração de código

--- pigeonholeSort.java
+++ pigeonholeSort.java
@@ -1,44 +1,27 @@
 public static void pigeonholeSort(int[] array) {
     // Array nulo ou com tamanho 0/1: ORDENADO
-    if (
-        (array == null) || 
-        (array.length == 0) || 
-        (array.length == 1)
-    ) return;
+    if (array == null || array.length <= 1) return;
 
-    // Descobrir Min e Max
-    int min = array[0];
-    int max = array[0];
-
+    // Descobrir menor e maior valor
+    int min = array[0], max = array[0];
     for (int i = 1; i < array.length; i++) {
         if (array[i] < min) min = array[i];
         if (array[i] > max) max = array[i];
     }
 
-    // Array auxiliar
-    int tamanhoCaixas = (max - min) + 1;
-    int[] caixas = new int[tamanhoCaixas]; // armazena quantos pombos tem em cada caixa
+    int[] caixas = new int[(max - min) + 1]; // armazena quantos pombos tem em cada caixa
 
     // Contar pombos nas caixas
-    int numAtual, indiceCaixa;
-    
     for (int i = 0; i < array.length; i++) {
-        numAtual = array[i];
-        indiceCaixa = numAtual - min;
-
-        caixas[indiceCaixa]++; // +1 pombo
+        caixas[array[i] - min]++; // +1 pombo
     }
 
-    // Ordenar o array
     int indiceOriginal = 0;
-    int valor;
-
-    for (int i = 0; i < tamanhoCaixas; i++) {
+    
+    // Ordenar o array
+    for (int i = 0; i < caixas.length; i++) {
         while(caixas[i] > 0) {
-            valor = i + min;
-            array[indiceOriginal] = valor;
-            
-            indiceOriginal++;
+            array[indiceOriginal++] = i + min;
             caixas[i]--;
         }
     }
